fbf4eb5f92b38b8f1746d621c3fa720a645e4f5c
GearHead87 <hosanulislam87@gmail.com>
2024-11-09 16:02:55 +0600

    book rental server

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+export const DatabaseConfig = {
+	databaseName: process.env.MYSQLDATABASE,
+	databaseHost: process.env.MYSQLDATABASE_HOST,
+	databaseUser: process.env.MYSQLDATABASE_USER,
+	databasePassword: process.env.MYSQLDATABASE_PASSWORD,
+	databasePort: 21384,
+};
